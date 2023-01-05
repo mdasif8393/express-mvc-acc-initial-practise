64a6582e92404e1c0334d46a6d9dbdb5d1fa2c7f
mdasif8393 <md.asif.8393@gmail.com>
2023-01-05 20:06:55 +0600

    add utils folder and connect database and sendEmail

--- index.js
+++ index.js
@@ -6,59 +6,13 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
-const nodemailer = require("nodemailer");
+
+const dbConnect = require("./utils/dbConnect");
 
 app.use(cors());
 app.use(express.json());
 
-
-const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: "",
-    pass: process.env.MAIL_PASS
-  }
-});
-function sendOrderMail(orders) {
-  const { address, name, email, product, price, quantity } = orders;
-
-
-
-  const mailOptions = {
-    from: "",
-    to: email,
-    subject: `Your order  ${product} is successfully booked`,
-    text: "Your product will be shifted within 7 days",
-    html: `
-      <div> 
-        <p>Hello, Sir</p>
-        <h4>Your product will be shifted in this address ${address}</h4>
-        <h4>Quantity: ${quantity}</h4>
-        <h4>Total Price: ${price*quantity} Tk BDT.</h4>
-        <p>Sincerely</p>
-        <p>Total Car Care Ltd. </p>
-        <h4 className="mt-5">Our Address</h4>
-        <p>Muradpur ,Ctg</p>
-        <p>Bangladesh</p>     
-     
-      </div>
-    `
-  };
-
-  transporter.sendMail(mailOptions, function (err, data) {
-    if (err) {
-      console.log("something is wrong", err);
-    } else {
-      // console.log('Email sent', data);
-    }
-  });
-
-}
-
-
-
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.roc0q.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+dbConnect();
 
 async function run() {
   try {
@@ -103,7 +57,7 @@ async function run() {
       const orders = req.body;
       const result = await orderCollection.insertOne(orders);
       console.log(orders);
-      sendOrderMail(orders);
+      sendMail(orders);
       res.send(result);
     });
     app.get("/orders", async (req, res) => {
